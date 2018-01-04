e65152df4cda4292717c69a0c420c58dc3cd35ae
apu <pasha_anik@ukr.net>
2018-01-04 19:51:39 +0200

    add start delay

--- ServerTest/AuctionMuptipleClientTestScript.bat
+++ ServerTest/AuctionMuptipleClientTestScript.bat
@@ -1,13 +1,14 @@
 @echo off
 
-set AMOUNT_OF_CLIENTS=20
+set AMOUNT_OF_CLIENTS=10
 
 set TEST_PATH=%cd%
+set SRC_PATH=%cd%
 
 set BAT_FILE_NAME=AuctionClientSingleRun.bat
 set BAT_FILE_CONTENT=start java -jar AuctionClient-1.0-jar-with-dependencies.jar
 
-set SRC_JAR_FILE_PATH=d:\Projects_java\AuctionClient\target\AuctionClient-1.0-jar-with-dependencies.jar
+set SRC_JAR_FILE_PATH=AuctionClient-1.0-jar-with-dependencies.jar
 set RESULT_JAR_FILE_NAME=AuctionClient-1.0-jar-with-dependencies.jar
 
 set LOG_FILE_NAME=AuctionClient.log
@@ -24,9 +25,11 @@ del %BAT_FILE_NAME%
 del %RESULT_JAR_FILE_NAME%
 
 echo %BAT_FILE_CONTENT% %%B 1 > %BAT_FILE_NAME%
-copy %SRC_JAR_FILE_PATH%  %RESULT_JAR_FILE_NAME% /Y
+copy ..\%SRC_JAR_FILE_PATH%  %RESULT_JAR_FILE_NAME% /Y
 
 call %BAT_FILE_NAME%
+
+timeout /t 1 /nobreak
 
 )
 
